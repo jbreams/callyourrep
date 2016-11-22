3a78b8221fbb0e8eb55a7e254de0e651092de4a1
Jonathan Reams <jbreams@gmail.com>
2016-11-21 22:56:13 -0500

    Working!

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, json
 from flask.ext.pymongo import PyMongo
 from twilio.util import TwilioCapability
 from twilio import twiml
+from datetime import datetime
 import phonenumbers
+from bson import json_util
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'callyourrep'
@@ -18,6 +20,14 @@ def getDistricts():
     districts = mongo.db.districts.find(query)
     return ")]}',\n" + json.dumps([ d for d in districts ])
 
+@app.route('/api/topics.json', methods=['GET'])
+def getTopics():
+    districtId = str(request.args.get('district'))
+    query = { '$or': [ {'district': districtId }, {'district': { '$exists': False }}],
+        '$or': [ { 'expires': { '$exists': False } }, { 'expires': {'$lt': datetime.now()}} ] }
+    topics = mongo.db.topics.find(query, {'$_id': 0 })
+    return ")]}',\n" + json_util.dumps([ t for t in topics ])
+
 @app.route('/api/inbound')
 def inbound():
     resp = twiml.Response()
@@ -26,6 +36,7 @@ def inbound():
         district = mongo.db.districts.find_one({'phone': phoneNumber})
         if district:
             parsed = phonenumbers.parse(phoneNumber, 'US')
+            dial.timeLimit(900)
             dial.number(phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.E164))
 
     return str(resp)
